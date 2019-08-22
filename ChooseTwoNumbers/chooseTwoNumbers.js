f69e2fe0f08220ba130f1a1867dd5b3f234fa8f7
xsami <samil.vargas@wirecard.com>
2019-08-22 13:57:41 -0400

    Doc for chooseTwoNumbers algorithm

--- ChooseTwoNumbers/chooseTwoNumbers.js
+++ ChooseTwoNumbers/chooseTwoNumbers.js
@@ -1,5 +1,15 @@
+// For more information check this link:
+// http://codeforces.com/problemset/problem/1206/A
+
 const FILLED = 1;
 
+/**
+ * Generates a dictionary with the information in the array
+ * received by parameter
+ *
+ * @param {*} array
+ * @returns {Map with the element of the array as keys} 
+ */
 function generateDict(array) {
     let res = {};
 
@@ -11,7 +21,12 @@ function generateDict(array) {
 
     return res;
 }
-
+/**
+ * Validates a key in a Map
+ * 
+ * @param {*} arguments that will be passed as a list
+ * @returns {Object} public methods of this class.  
+ */
 function Validator() {
     var parameters = [...arguments];
     return {
@@ -26,6 +41,14 @@ function Validator() {
     }
 }
 
+/**
+ * Finds if there are two numbers that added does not match
+ * with any element of the two array
+ *
+ * @param {Array} A
+ * @param {Array} B
+ * @returns {Object} the numbers that can be added together
+ */
 function chooseTwoNumbers(A, B) {
 
     const myValidator = new Validator(generateDict(A), generateDict(B));
@@ -45,4 +68,5 @@ function chooseTwoNumbers(A, B) {
     return null;
 }
 
+// Section: Test Cases
 console.log(chooseTwoNumbers([-1, 3, 5, 7], [7, 5, 3, 2])); 
