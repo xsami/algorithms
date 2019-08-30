23428a1c62bd5d1c13b4c45d3d1fc6688b6fb9ba
xsami <samil.vargas@hotmail.com>
2019-08-30 16:53:50 -0400

    Fixing bug, nil pointer exception, obj reference

--- SLinkedList/sLinkedList.go
+++ SLinkedList/sLinkedList.go
@@ -2,20 +2,18 @@ package main
 
 import "fmt"
 
-var NullObject = &Node{}
-
 type Node struct {
-	Next *Node
+	Next  *Node
 	Value interface{}
 }
 
 func (n *Node) find(val interface{}) (int, error) {
-	
+
 	cur := n
 	i := 0
-	
+
 	for {
-		if cur == NullObject {
+		if cur == nil {
 			break
 		}
 
@@ -26,16 +24,16 @@ func (n *Node) find(val interface{}) (int, error) {
 		cur = cur.Next
 		i++
 	}
-	
-	return -1, fmt.Errorf("Not founded")
+
+	return -1, fmt.Errorf("Element not found")
 }
 
 func (n *Node) insert(val interface{}) {
-
+	
 	cur := n
 
 	for {
-		if cur.Next == NullObject {
+		if cur.Next == nil {
 			break
 		}
 		cur = cur.Next
@@ -46,28 +44,33 @@ func (n *Node) insert(val interface{}) {
 
 }
 
-
+// TODO: Enhance for i < val
 func (n *Node) delete(val interface{}) (int, error) {
-	
-	if val, err := n.find(val); err != nil {
-		return val, err
+
+	if idx, err := n.find(val); err != nil {
+		return idx, err
 	}
 
 	i := 0
 	cur := n
 
+	if cur.Value == val {
+		cur = &Node{}
+		return 0, nil
+	}
+
 	for {
 
-		if cur.Next == NullObject {
+		if cur.Next == nil {
 			break
 		}
 
 		if cur.Next.Value == val {
 
-			if cur.Next.Next != NullObject {
+			if cur.Next.Next != nil {
 				cur.Next = cur.Next.Next
 			} else {
-				cur.Next = NullObject
+				cur.Next = nil
 			}
 
 			return i, nil
@@ -79,7 +82,29 @@ func (n *Node) delete(val interface{}) (int, error) {
 	return -1, fmt.Errorf("Something wrong happened")
 }
 
-
 func main() {
+
+	myNode := &Node{
+		Value: 1}
+	myNode.insert(2)
+	myNode.insert(3)
+	myNode.insert(4)
+	myNode.insert(5)
+	myNode.insert(6)
+
+	if idx, err := myNode.find(3); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("Index: %v\n", idx)
+	}
+	
+	myNode.delete(3)
+	
+	if idx, err := myNode.find(3); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("Index: %v\n", idx)
+	}
+
 	fmt.Println("Hello World")
 }
