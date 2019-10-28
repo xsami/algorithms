d206b255b3b13f94916de1d0bddee53288e63421
xsami <samil.vargas@hotmail.com>
2019-10-28 16:55:39 -0400

    save the princess problem to practice

---
+++ SaveThePrincess/savePrincess.py
@@ -0,0 +1,17 @@
+# Find the Princess Peach
+
+# Starting at the position (x, y) and given a table of size
+# N x N your task is to find wheather is a possible path where
+# you can traversal the grid and point on the same position of 
+# the princess. Print Yes or No wheather is possible or not to
+# save the princess.
+
+# Constraints: 
+
+class Position(self, x, y):
+    self.x = x
+    self.y = y
+
+
+# TODO: implemet the can save princess method
+def canSavePrincess(me, table):
