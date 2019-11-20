64b0c67e0ac571a0d4de5f31f42d68bfa021e719
xsami <samil.vargas@hotmail.com>
2019-11-20 15:41:45 -0400

    BirthdayCake candler problem solved from Hackerrank

---
+++ CakeCandler/cakeCandler.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+
+func birthdayCakeCandles(ar []int32) int32 {
+
+    var res int32 = 1
+    var max int32 = -1
+
+    for _, val := range ar {
+        if val > max {
+            max = val
+            res = 1
+        } else if val == max {
+            res++
+        }
+    } 
+
+
+    return res
+
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 1024 * 1024)
+
+    arCount, err := strconv.ParseInt(readLine(reader), 10, 64)
+    checkError(err)
+
+    arTemp := strings.Split(readLine(reader), " ")
+
+    var ar []int32
+
+    for i := 0; i < int(arCount); i++ {
+        arItemTemp, err := strconv.ParseInt(arTemp[i], 10, 64)
+        checkError(err)
+        arItem := int32(arItemTemp)
+        ar = append(ar, arItem)
+    }
+
+    result := birthdayCakeCandles(ar)
+
+    fmt.Fprintf(writer, "%d\n", result)
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
