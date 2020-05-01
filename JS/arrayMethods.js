d18945d7e6ad916e2d4b7a631ef2f2614400aa14
xsami <samil.vargas@hotmail.com>
2020-05-01 14:02:29 -0400

    completing array methods

--- JS/arrayMethods.js
+++ JS/arrayMethods.js
@@ -17,7 +17,7 @@ arrForEach.customForEach(e => console.log(e));
 
 // .some() function loop through the array and return false if any of the value doesnt meet
 // the callback argument
-Array.prototype.customSome = function(callback) {
+Array.prototype.customEvery = function(callback) {
     for (let i = 0; i < this.length; i++) {
         if (!callback(this[i])) {
             return false;
@@ -27,6 +27,56 @@ Array.prototype.customSome = function(callback) {
 };
 
 // Test output
+const arrEvery = [1, 2, 3, 4, 5, 6];
+console.log( arrEvery.every(e => e % 2) );
+console.log( arrEvery.customEvery(e => e % 2) ); // this will return false
+
+
+// .some() function tries to find is there's one element who meets the condition
+// passed in the callback argument
+Array.prototype.customSome = function(callback) {
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i])) {
+            return true;
+        }
+    }
+    return false;
+};
+
+// Test output
 const arrSome = [1, 2, 3, 4, 5, 6];
 console.log( arrSome.some(e => e % 2) );
-console.log( arrSome.customSome(e => e % 2) );
+console.log( arrSome.customSome(e => e % 2) ); // this will return true
+
+
+
+// .map() function mutates the array with the function passed by parameter
+Array.prototype.customMap = function(callback) {
+    for (let i = 0; i < this.length; i++) {
+        this[i] = callback(this[i], i, this);
+    }
+    return this;
+};
+
+
+// Test output
+const arrMap = [1, 2, 3, 4, 5, 6];
+console.log( arrMap.map(e => e * 2) );
+console.log( arrMap.customMap(e => e / 2) );
+
+
+// .filter() return an array with the values that matched the value from the callback
+Array.prototype.customFilter = function(callback) {
+    const result = [];
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i])) {
+            result.push(this[i]);
+        }
+    }
+    return result;
+};
+
+// Test output
+const arrFilter = [1, 2, 3, 4, 5, 6];
+console.log( arrFilter.filter(e => e % 2) );
+console.log( arrFilter.customFilter(e => e % 2) );
