51b1339ca966bfc6b4b90f9b25617e907cbf5070
xsami <samil.vargas@hotmail.com>
2020-05-09 15:36:09 -0400

    markdown dparser problem description

--- MarkdownDParser/markdownDParser.js
+++ MarkdownDParser/markdownDParser.js
@@ -1,45 +1,5 @@
-/**
- * Task: You're assigned to build a markdown deparser.
- * Markdown is text format that is made to display text
- * to HTML. As the current text if formatted to html we
- * would like to covert this to markdown. For more reference
- * about markdown you can check the wiki documentation: 
- * https://en.wikipedia.org/wiki/Markdown for this.
- * 
- * Requirement: given a HTML string you must return the
- * markdown equivalent for this.
- * 
- * The output for the following example input is:
- *      # Sport News
- *      #### With the current information about the sports in the world
- *      The **foundation** that _Denzel Ward_ established after reaching the NFL as a first-round pick with his hometown Cleveland Browns is dubbed Make Them Know Your Name (MTKYN). The term is a tribute to Ward’s late father, Paul, who repeated that inspirational mantra as his son emerged as a star athlete.
- *      Favorite Sports in **DR**:
- *          * Basketball
- *          * Softball
- *          * Baseball
- *          * Soccer
- * 
- * Note: We would probrably like to also implement a function that converts
- * markdown to HTML so it would be usefull to keep this in mind for the current
- * implementation of your solution
- */
-
-const exampleHTML = `
-<h1>Sport News</h1>
-<h4>With the current information about the sports in the world</h4>
-<p>The <strong>foundation</strong> that <em>Denzel Ward</em> established after reaching the NFL as a first-round pick with his hometown Cleveland Browns is dubbed Make Them Know Your Name (MTKYN). The term is a tribute to Ward’s late father, Paul, who repeated that inspirational mantra as his son emerged as a star athlete.</p>
-<p>Favorite Sports in <strong>DR</strong>:</p>
-<ul>
-<li>Basketball</li>
-<li>Softball</li>
-<li>Baseball</li>
-<li>Soccer</li>
-</ul>
- `;
 
 // TODO: complete the deparser function
 function markownDParser(htmlText) {
      return;
 }
-
-console.log(markownDParser(exampleHTML));
