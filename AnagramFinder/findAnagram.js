4b31482a17a0d8addca29c2a1fa4e6650f365204
xsami <samil.vargas@hotmail.com>
2020-05-09 14:03:14 -0400

    find anagram problem refactoring

--- AnagramFinder/findAnagram.js
+++ AnagramFinder/findAnagram.js
@@ -1,43 +1,12 @@
-function getCharAmount(w='') {
-    const r = {};
-    for (let  i = 0; i < w.length; i++) {
-        if (r[w[i]] === undefined) {
-            r[w[i]] = 1;
-        } else {
-            r[w[i]]++;
-        }
-    }
-    return r;
-}
-
-function matchObj(original={}, toCompare={}) {
-    for (let i in original) {
-        if (original[i] !== toCompare[i]) {
-            return false;
-        }
-    }
-    return true;
+function getSortedWord(word) {
+    return word.split('').sort().join('');
 }
 
 function getAnagram(w='', list=[]) {
-
-    const res = [];
-    const c1 = getCharAmount(w);
-    let c2 = [];
-
-    list.filter((wl) => wl.length === w.length); // Just filter elements that the length are equal to w (original word)
-
-    list.forEach((element) => {
-        c2 = getCharAmount(element);
-
-        if (matchObj(c1, c2)) {
-            res.push(element);
-        }
-    });
-
-    return res;
+    const sortedWord = getSortedWord(w.toLowerCase());
+    return list.filter(element => getSortedWord(element.toLowerCase()) === sortedWord);
 }
 
-console.log(getAnagram('horse', ['shore', 'esrho', 'dinner', 'abdop', 'roseh']) );
+console.log(getAnagram('horse', ['shore', 'esrho', 'dinner', 'abdop', 'roseh']));
 console.log(getAnagram('podab', ['shore', 'esrho', 'dinner', 'abdop', 'roseh']));
 console.log(getAnagram('bitch', ['shore', 'esrho', 'dinner', 'abdop', 'roseh']));
