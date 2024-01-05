5e94de03b825f220b5d6d29cffcdaec9c3b135f7
xsami <samil.vargas@nytimes.com>
2024-01-05 00:53:50 -0400

    power X to the N

---
+++ PowXN/powXN.exs
@@ -0,0 +1,7 @@
+# Solution for: https://leetcode.com/problems/powx-n/description/
+defmodule Solution do
+  @spec my_pow(x :: float, n :: integer) :: float
+  def my_pow(x, n) do
+    x ** n
+  end
+end
