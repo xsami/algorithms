532ff103e87c10badf6efce8840d53045a705bfc
xsami <samil.vargas@nytimes.com>
2024-01-08 00:04:56 -0400

    fix money bank solution

--- MoneyBank/moneyBank.exs
+++ MoneyBank/moneyBank.exs
@@ -1,9 +1,14 @@
+# Solution for: https://leetcode.com/problems/calculate-money-in-leetcode-bank/description/
 defmodule Solution do
 
   defp get_sum(n, -1), do: 0
 
   defp get_sum(n, r) do
     n * (4 + r) + get_sum(n, r-1)
+  end
+
+  defp get_sum_a_to_b(a, b) do
+    div(a * (a + 1), 2) - div(b * (b+ 1), 2)
   end
 
   @spec total_money(n :: integer) :: integer
@@ -14,7 +19,7 @@ defmodule Solution do
     if r == 0 do
         get_sum(7, d - 1)
     else
-        get_sum(7, d - 1) + div(t * (t + 1), 2) - d
+        get_sum(7, d - 1) + get_sum_a_to_b(t, d)
     end
   end
 end
